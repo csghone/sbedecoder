189bc38b620ed145648d7e9c881b495c1bb259a5
Chaitanya Ghone <csghone@acceletrade.com>
2017-12-01 16:38:34 +0530

    Work with latest version from github
    Signed-off-by: Chaitanya Ghone <csghone@acceletrade.com>

--- scripts/mdp_raw_decoder.py
+++ scripts/mdp_raw_decoder.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 """
 Parse a pcap file containing CME MDP3 market data based on a SBE xml schema file.
 """
 
+import argparse
 import sys
 import os.path
 from struct import unpack
@@ -14,152 +17,273 @@ from sbedecoder import SBEMessageFactory
 from sbedecoder import SBEParser
 import gzip
 import dpkt
+import logging
+import logging.handlers
+import subprocess
+import traceback
+import progressbar
 
-seq_num = 0;
-seq_num1 = 0;
-loop_start = 0;
-def parse_mdp3_packet(mdp_parser,  data, skip_fields):
-    # parse the packet header: http://www.cmegroup.com/confluence/display/EPICSANDBOX/MDP+3.0+-+Binary+Packet+Header
-    global seq_num
-    global loop_start
-    sequence_number = unpack_from("<i", data, offset=0)[0]
-    sending_time = unpack_from("<Q", data, offset=4)[0]
-    # if sequence_number ==1 and loop_start == 0:
-    #     loop_start = 1
-    # elif sequence_number != 1 and loop_start == 0:
-    #     return
-    # elif sequence_number == 1 and loop_start == 1:
-    #     sys.exit(0)
+if __name__ == "__main__":
+    # Logging setup
+    logger = logging.getLogger()
 
-    print(':packet -  sequence_number: {} sending_time: {} '.format(sequence_number, sending_time))
-    # try:
-    #     if seq_num > 0:
-    #         if((seq_num +1) != sequence_number):
-    #             sys.exit(0)
-    #     seq_num = sequence_number;
-    #     print(sequence_number);
-    # except Exception as e:
-    #     print e;
-    #     raise
-    for mdp_message in mdp_parser.parse(data, offset=12):
-        checker = False;
-        # if mdp_message.template_id.value == 32 or mdp_message.template_id.value == 42 or mdp_message.template_id.value == 38 :
-        #     checker = True;
-        # if mdp_message.template_id.value == 38:
-        #      checker = False;
-        # if mdp_message.template_id.value == 38:
-        #     security_id = mdp_message.security_id.value
-        #     if 160078 == security_id:
-        #          checker = True;
-        if mdp_message.template_id.value == 32 or mdp_message.template_id.value == 42 or mdp_message.template_id.value == 43:
+    LOG_FORMATTER = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - " +
+        "%(lineno)s - %(funcName)s - " +
+        "%(message)s")
+else:
+    logger = logging.getLogger(__name__)
+
+
+def setup_logging(level=logging.ERROR):
+    file_log_handler = logging.handlers.RotatingFileHandler(
+        __name__ + ".log",
+        maxBytes=10000000,
+        backupCount=5)
+    console_log_handler = logging.StreamHandler()
+    logger.addHandler(file_log_handler)
+    logger.addHandler(console_log_handler)
+    logger.setLevel(level)
+    for handler in logging.root.handlers:
+        handler.setFormatter(fmt=LOG_FORMATTER)
+
+
+class MDP3Parser:
+    def __init__(self, schema):
+        self.seq_num = 0
+        # Read in the schema xml as a dictionary and
+        # construct the various schema objects
+        mdp_schema = SBESchema()
+        mdp_schema.parse(schema)
+        msg_factory = SBEMessageFactory(mdp_schema)
+        self.mdp_parser = SBEParser(msg_factory)
+        # self.loop_start = 0
+
+    def handle_repeating_groups(self, group_container, msg_version, indent,
+                                skip_fields=[], secdef=None):
+        for group in group_container.groups:
+            if group.since_version > msg_version:
+                continue
+            print(':::{} - num_groups: {}'.format(group.name, group.num_groups))
+            for group_field in group.repeating_groups:
+                group_fields = ""
+                for group_field in group_field.fields:
+                    if group_field.since_version > msg_version:
+                        continue
+                    group_fields += "\n" + " "*len(indent)
+                    if secdef and group_field.id == '48':
+                        security_id = group_field.value
+                        symbol_info = secdef.lookup_security_id(security_id)
+                        if symbol_info:
+                            symbol = symbol_info[0]
+                            group_fields += "security_id: {} [{}]".format(
+                                security_id, symbol)
+                            continue
+                    group_fields += str(group_field)
+                print("::::{}".format(group_fields))
+            self.handle_repeating_groups(
+                group,
+                msg_version,
+                indent + ':',
+                skip_fields=skip_fields,
+                secdef=secdef
+            )
+
+
+    def parse_packet(self, data, skip_fields=[], token_filter=[]):
+        seq_num_str = ":packet => sequence_number: {} sending_time: {}"
+        # Parse the packet header:
+        # http://www.cmegroup.com/confluence/display/EPICSANDBOX/MDP+3.0+-+Binary+Packet+Header
+
+        sequence_number = unpack_from("<i", data, offset=0)[0]
+        sending_time = unpack_from("<Q", data, offset=4)[0]
+        # if sequence_number ==1 and loop_start == 0:
+        #     loop_start = 1
+        # elif sequence_number != 1 and loop_start == 0:
+        #     return
+        # elif sequence_number == 1 and loop_start == 1:
+        #     sys.exit(0)
+
+        # print(seq_num_str.format(sequence_number, sending_time))
+
+        # try:
+        #     if seq_num > 0:
+        #         if((seq_num +1) != sequence_number):
+        #             sys.exit(0)
+        #     seq_num = sequence_number;
+        #     print(sequence_number);
+        # except Exception as e:
+        #     print e;
+        #     raise
+
+        template_id_filter = [32, 42, 43]
+        for mdp_message in self.mdp_parser.parse(data, offset=12):
+            template_val = mdp_message.template_id.value
+            if template_val not in template_id_filter:
+                continue
+
+            checker = False
+            if not token_filter:
+                checker = True
             for md_entry in mdp_message.no_md_entries:
                 security_id = md_entry.security_id.value
-                if 66404 == security_id:
+                if token_filter is not None and security_id in token_filter:
                     checker = True
-                # if md_entry.md_entry_type.value not in ['Bid', 'Offer']:
-                #     checker = False
-        if mdp_message.template_id.value == 36:
-            checker = True
-        checker = True
-        # if mdp_message.template_id.value == 12:
-        #    checker = False
-        if checker == True:
-            print('=======================================================================================')
-            print(':packet -  sequence_number: {} sending_time: {} '.format(sequence_number, sending_time))
-            message_fields = ''
+
+            message_fields = ""
             for field in mdp_message.fields:
                 if field.name not in skip_fields:
-                    message_fields += ' ' + str(field)
-            print('::{} - {}'.format(mdp_message, message_fields))
-            for iterator in mdp_message.iterators:
-                print(':::{} - num_groups: {}'.format(iterator.name, iterator.num_groups))
-                for index, group in enumerate(iterator):
-                    group_fields = ''
-                    for group_field in group.fields:
-                        group_fields += str(group_field) + ' '
-                    print('::::{}'.format(group_fields))
-        # elif mdp_message.template_id.value == 38:
-        #     security_id = mdp_messgae.security_id.value
-        #     if 160078 == security_id:
-        #         message_fields = ''
-        #         for field in mdp_message.fields:
-        #             if field.name not in skip_fields:
-        #                 message_fields += ' ' + str(field)
-        #         print('::{} - {}'.format(mdp_message, message_fields))
-        #         for iterator in mdp_message.iterators:
-        #             print(':::{} - num_groups: {}'.format(iterator.name, iterator.num_groups))
-        #             for index, group in enumerate(iterator):
-        #                 group_fields = ''
-        #                 for group_field in group.fields:
-        #                     group_fields += str(group_field) + ' '
-        #                 print('::::{}'.format(group_fields))
+                    message_fields += "\n  " + str(field)
+
+            if not checker:
+                continue
+
+            print(token_filter, checker)
+            print("=" * 90)
+            print(seq_num_str.format(sequence_number, sending_time))
+            print("::{} -{}\n".format(mdp_message, message_fields))
+
+            try:
+                # Code for older version
+                _ = mdp_message.iterators
+                for iterator in mdp_message.iterators:
+                    print(
+                        ":::{} - num_groups: {}\n".format(
+                            iterator.name,
+                            iterator.num_groups
+                        )
+                    )
+                    group_fields = ""
+                    for index, group in enumerate(iterator):
+                        for group_field in group.fields:
+                            group_fields += "\n    " + str(group_field)
+                        print('::::{}'.format(group_fields))
+            except:
+                self.handle_repeating_groups(
+                    mdp_message,
+                    mdp_message.version.value,
+                    indent="::::",
+                    skip_fields=skip_fields,
+                    secdef=None
+                )
+                continue
 
 
-def process_raw_file(args, filename):
-    # Read in the schema xml as a dictionary and construct the various schema objects
-    mdp_schema = SBESchema()
-    mdp_schema.parse(args.schema)
-    msg_factory = SBEMessageFactory(mdp_schema)
-    mdp_parser = SBEParser(msg_factory)
 
-    skip_fields = set(args.skip_fields.split(','))
+def process_raw_file(args):
+    ret_val = 0
+    filename = args.input_file
 
-    with gzip.open(filename, 'rb') if filename.endswith('.gz') else open(filename, 'rb') as file:
-        while True:
-            timestamp_count_bytes = file.read(1)
-            timestamp_count, = unpack('B', timestamp_count_bytes)
-            # print(timestamp_count)
-            for i in range(0,timestamp_count):
-                timestamps_bytes = file.read(8)
-                timestamp, = unpack('L', timestamps_bytes)
-                # print(timestamp)
-            size_bytes = file.read(4)
-            if size_bytes:
-                chunk_size, = unpack('I', size_bytes)
-                # print(chunk_size)
-                chunk = file.read(chunk_size)
-                try:
-                    parse_mdp3_packet(mdp_parser, chunk, skip_fields)
-                except Exception:
-                    print('could not parse packet number {}'.format(packet_number))
-            else:
-                break;
+    skip_fields = args.skip_fields.split(',')
+    token_filter = args.token_filter.split(',')
+    if token_filter == [""]:
+        token_filter = []
+    token_filter = [int(x) for x in token_filter]
 
+    if filename.endswith(".gz"):
+        file_handle = gzip.open(filename, "rb")
+    else:
+        file_handle = open(filename, "rb")
+
+    mdp3_parser = MDP3Parser(args.schema)
+
+    file_size = os.stat(filename).st_size
+    bar = progressbar.ProgressBar()
+    while True:
+        bar_val = 100.0 * float(file_handle.tell()) / file_size
+        bar.update(bar_val)
+
+        timestamp_count_bytes = file_handle.read(1)
+        timestamp_count, = unpack('B', timestamp_count_bytes)
+        logger.debug("Timestamp byte length: %s", str(timestamp_count))
+
+        for i in range(0, timestamp_count):
+            timestamps_bytes = file_handle.read(8)
+            timestamp, = unpack('L', timestamps_bytes)
+            logger.debug("Timestamp: %s", str(timestamp))
+
+        size_bytes = file_handle.read(4)
+        if size_bytes:
+            chunk_size, = unpack('I', size_bytes)
+            logger.debug("Chunk size: %d", chunk_size)
+            chunk = file_handle.read(chunk_size)
+            try:
+                mdp3_parser.parse_packet(chunk, skip_fields, token_filter)
+            except Exception as error:
+                exc_mesg = traceback.format_exc()
+                logger.error("\n%s", exc_mesg)
+                logger.error("Error %s", error)
+                ret_val = -1
+                break
+        else:
+            break
+
+    file_handle.close()
+    return ret_val
 
 
 def process_command_line():
-    from argparse import ArgumentParser
+    parser = argparse.ArgumentParser(
+        description="Parse CME MDP3 market data",
+        version="0.2"
+    )
 
-    parser = ArgumentParser(
-        description="Parse a pcap file containing CME MDP3 market data based on a SBE xml schema file.",
-        version="0.1")
+    parser.add_argument(
+        "--input",
+        dest="input_file",
+        help="Input file to process"
+    )
 
-    parser.add_argument("pcapfile",
-        help="Name of the pcap file to process")
+    parser.add_argument(
+        "--schema",
+        dest="schema",
+        default='templates_FixBinary.xml',
+        help="Path to SBE schema xml file"
+    )
 
-    parser.add_argument("-s", "--schema", default='templates_FixBinary.xml',
-        help="Name of the SBE schema xml file")
+    default_skip_fields = [
+        "message_size",
+        "block_length",
+        "template_id",
+        "schema_id",
+        "version"
+    ]
 
-    default_skip_fields = 'message_size,block_length,template_id,schema_id,version'
+    parser.add_argument(
+        "--skip_fields",
+        dest="skip_fields",
+        default=",".join(default_skip_fields),
+        help="comma separated list of fields to skip. default={}".format(
+            ",".join(default_skip_fields)
+        )
+    )
 
-    parser.add_argument("-f", "--skip-fields", default=default_skip_fields,
-        help="Don't print these message fields (default={})".format(default_skip_fields))
+    parser.add_argument(
+        "--token_filter",
+        dest="token_filter",
+        default="",
+        help="comma separated list of venue tokens to use. Default: all"
+    )
 
     args = parser.parse_args()
 
     # check number of arguments, verify values, etc.:
     if not os.path.isfile(args.schema):
-        parser.error("sbe schema xml file '{}' not found".format(args.schema))
+        logger.info("Downloading sbe schema xml file from the web")
+        command = "wget ftp://ftp.cmegroup.com/SBEFix/Production/Templates/templates_FixBinary.xml"
+        subprocess.call(command, shell=True)
+
+    if not os.path.isfile(args.input_file):
+        parser.error("Cannot find file '{}' not found".format(args.input_file))
 
     return args
 
 
 def main(argv=None):
     args = process_command_line()
-    process_raw_file(args, args.pcapfile)
-    return 0  # success
+    return process_raw_file(args)
 
 
 if __name__ == '__main__':
-    status = main()
-    sys.exit(status)
+    setup_logging(level=logging.INFO)
+    sys.exit(main())
